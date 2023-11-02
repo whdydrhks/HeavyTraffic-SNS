69d65d2fab1a4936b206cf87513d81f7b2b74fd7
whdydrhks <whdydrhks@naver.com>
2023-11-02 11:36:33 +0900

    회원정보 변경, 테스트

--- src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
@@ -29,7 +29,13 @@ public class Member {
         this.createdAt = createAt == null ? LocalDateTime.now() : createAt;
     }
 
-    void validateNickName(String nickname) {
+    public void changeNickname(String to) {
+        Objects.requireNonNull(to);
+        validateNickName(to);
+        nickname = to;
+    }
+
+    private void validateNickName(String nickname) {
         Assert.isTrue(nickname.length()<=NAME_MAX_LENGTH, "최대 길이를 초과했습니다.");
     }
 }
---
+++ src/test/java/com/example/fastcampusmysql/domain/member/MemberTest.java
@@ -0,0 +1,33 @@
+package com.example.fastcampusmysql.domain.member;
+
+import com.example.fastcampusmysql.util.MemberFixtureFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class MemberTest {
+    @DisplayName("회원은 닉네임을 변경할 수 있다.")
+    @Test
+    public void testChangeName() {
+        var member = MemberFixtureFactory.create();
+        var expected = "ygjo";
+
+        member.changeNickname(expected);
+        Assertions.assertEquals(expected, member.getNickname());
+    }
+
+    @DisplayName("회원의 닉네임은 10자를 초과할 수 없다.")
+    @Test
+    public void testNicknameMaxLength() {
+        var member = MemberFixtureFactory.create();
+        var overMaxLengthName = "ygjoygjoygjo";
+
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> member.changeNickname(overMaxLengthName)
+        );
+    }
+}
