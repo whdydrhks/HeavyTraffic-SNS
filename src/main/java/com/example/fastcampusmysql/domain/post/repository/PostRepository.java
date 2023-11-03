709d20fc67204470448a70b7a999c61c3bea0b12
whdydrhks <whdydrhks@naver.com>
2023-11-03 13:49:54 +0900

    Post 중간

--- src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.fastcampusmysql.domain.post.repository;
 
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.entity.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -8,11 +10,24 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Repository
 public class PostRepository {
     static final String TABLE = "Post";
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public List<DailyPostCount> groupByCreateDate(DailyPostCountRequest request) {
+        var sql = """
+                    SELECT createdDate, memberId, count(id)
+                    FROM %s
+                    WHERE memberId = :memberId and createdDate between :firstDate and lastDate
+                    GROUP BY memberId, createdDate
+                  """;
+        var params = new BeanPropertySqlParameterSource(request);
+        return namedParameterJdbcTemplate.query(sql, params,  )
+    }
     public Post save(Post post) {
         if(post.getId() == null)
             return insert(post);
