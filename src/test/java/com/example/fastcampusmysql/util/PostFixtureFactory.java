f5e88f4a225903ea13f5de267ecd089a42726316
whdydrhks <whdydrhks@naver.com>
2023-11-06 12:20:46 +0900

    POST(게시물) TEST 데이터 만들기

---
+++ src/test/java/com/example/fastcampusmysql/util/PostFixtureFactory.java
@@ -0,0 +1,29 @@
+package com.example.fastcampusmysql.util;
+
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+
+import java.time.LocalDate;
+
+import static org.jeasy.random.FieldPredicates.*;
+
+public class PostFixtureFactory {
+    static public EasyRandom get(Long memberId, LocalDate firstDate, LocalDate lastDate) {
+        // EasyRandom 객체를 만들어주고, 외부에서 EasyRandom 루프를 돌면서 nextObject 호출하는 방식
+        var idPredicate = named("id")
+                .and(ofType(Long.class))
+                .and(inClass(Post.class));
+
+        var memberIdPredicate = named("memberId")
+                .and(ofType(Long.class))
+                .and(inClass(Post.class));
+
+        var param = new EasyRandomParameters()
+                .excludeField(idPredicate)
+                .dateRange(firstDate, lastDate)
+                .randomize(memberIdPredicate, () -> memberId);
+
+        return new EasyRandom(param);
+    }
+}
